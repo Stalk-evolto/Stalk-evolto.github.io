b436b0581ead7ccce385690b06dcd405d17f6983
Stalk-evolto <84966392+Stalk-evolto@users.noreply.github.com>
2024-11-06 00:18:30 +0800

    Site updated: 2024-11-06 00:18:30

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730773897910";
+const VERSION = "1730823508319";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
